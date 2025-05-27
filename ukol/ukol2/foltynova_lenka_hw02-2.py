3f8077669c1518bf8f8937182cb6c0a793028e06
123Lenka <lenka.foltynova@gmail.com>
2025-05-27 19:35:49 +0200

    Finalni upravy kodu

--- ukol/ukol2/foltynova_lenka_hw02-2.py
+++ ukol/ukol2/foltynova_lenka_hw02-2.py
@@ -8,32 +8,30 @@ INDEX_STARTYEAR = 5
 
 
 movies = []
-data_rows = []
-movie = {}
+
 
 with open("netflix_titles.tsv", encoding="utf-8") as file:
     text = file.readlines()
 
-for line in text:
-    lines = line.strip().split('\t')
-    data_rows.append(lines)       
-    for row in data_rows[1:]:
-        movie['title'] = row[INDEX_PRIMARYTITLE]
-        movie['directors'] = row[INDEX_DIRECTOR].split(',')
-        if movie['directors'] == [""]:
-            movie['directors'] = []
-        else:
-            movie['directors'] = movie['directors']
-            
-        movie['cast'] = row [INDEX_CAST].split(',')
-        if movie['cast'] == [""]:
-            movie['cast'] = []
-        else:
-            movie['cast'] = movie['cast']
+for line in text[1:]:
+    items = line.strip().split('\t')
+    movie = {}
+    movie['title'] = items[INDEX_PRIMARYTITLE]
+    if not items [INDEX_DIRECTOR]:   
+        movie['director'] = []        
+    else:
+       movie['director'] = items[INDEX_DIRECTOR].split(', ')
+    
+    if not items [INDEX_CAST]:   
+        movie['cast'] = []        
+    else:
+       movie['cast'] = items[INDEX_CAST].split(', ')
 
-        movie['genres'] = row[INDEX_GENRES].split(',')
-        movie['decade'] = int(row[INDEX_STARTYEAR]) //10 * 10
-        movies.append(movie)
+    movie['genres'] = items[INDEX_GENRES].split(',')
+    movie['decade'] = int(items[INDEX_STARTYEAR]) //10 * 10
+    movies.append(movie)
 
 with open('hw02_output.json', mode='w', encoding='utf-8') as file:
     json.dump(movies, file, indent=4, ensure_ascii=False)
+
+
